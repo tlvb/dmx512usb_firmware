1d6ec22966834cb5504e44820cf258787752e361
Leo Bärring <leo.barring@gmail.com>
2012-07-11 00:36:41 +0200

    reorganization/cleanup
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- fw/main.c
+++ fw/main.c
@@ -1,35 +1,18 @@
-#include "board.h"
 #include "usbusart.h"
-#include <string.h>
+#ifndef F_CPU
+#define F_CPU 20000000
+#endif
 #include <util/delay.h>
 
-extern volatile RINGBUFFER(TXBSZ) txb;
-
 int main(void) {
-
-	pinsetup();
-
-	ledhello();
-
-	uu_init();
-
+	uu_setup();
 	sei();
 
 	uint8_t text[3];
-
 	
 	for (;;) {
-		LED_ON(LED0);
 		uu_read(text, 3);
-		LED_OFF(LED0);
 		uu_write(text, 3);
 		_delay_ms(125);
-
-	}
-}
-
-ISR(BAD_ISR_vect) {
-	for (;;) {
-		ledhello();
 	}
 }
