f2d929b9cd109354638fd98334993e2534168d12
Leo Bärring <leo.barring@gmail.com>
2012-07-10 23:05:37 +0200

    reorganization
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

---
+++ fw/leds.h
@@ -0,0 +1,21 @@
+#ifndef __LEDS_H__
+#define __LEDS_H__
+
+#include <avr/io.h>
+#define F_CPU 20000000
+#include <util/delay.h>
+
+#define LEDDDR DDRB
+#define LEDPORT PORTB
+#define LED2 PB7
+#define LED1 PB6
+#define LED0 PB5
+
+#define leds_on LEDPORT &=~ _BV(LED2)|_BV(LED1)|_BV(LED0)
+#define leds_off LEDPORT |= _BV(LED2)|_BV(LED1)|_BV(LED0)
+#define led_on(x) LEDPORT &=~ _BV(x)
+#define led_off(x) LEDPORT |= _BV(x)
+
+inline void led_setup(void);
+void led_hello(void);
+void led_error(void);
