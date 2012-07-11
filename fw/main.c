883bae0a1ce02634576152f4d6350cff98c7a4da
Leo Bärring <leo.barring@gmail.com>
2012-07-11 12:30:02 +0200

    led read wait notification added
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- fw/main.c
+++ fw/main.c
@@ -1,17 +1,22 @@
 #include "usbusart.h"
+#include "leds.h"
 #ifndef F_CPU
 #define F_CPU 20000000
 #endif
 #include <util/delay.h>
 
 int main(void) {
+	led_setup();
+	led_hello();
 	uu_setup();
 	sei();
 
 	uint8_t text[3];
 	
 	for (;;) {
+		led_on(LED0);
 		uu_read(text, 3);
+		led_off(LED1);
 		uu_write(text, 3);
 		_delay_ms(125);
 	}
