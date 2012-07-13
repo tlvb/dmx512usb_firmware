22d6f5480a4afc00d2f5c5d4f533bce484bffac8
Leo Bärring <leo.barring@gmail.com>
2012-07-13 16:41:26 +0200

    High speed dumping from the dmx input to the usb 16 cycles/sample @20MHz fcpu -> 800ns sample interval -> 5 samples/dmx bit something is not working though; ze leds, zey do nothing ...merge with known working code imminent Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- fw/main.c
+++ fw/main.c
@@ -1,7 +1,76 @@
-#include "board.h"
+#include <avr/io.h>
+#include <stdint.h>
+
+#define DMXDDR DDRD
+#define DMXPORT PORTD
+#define DMXPIN PIND
+
+#define DMXREN PD5	/* o */
+#define DMXTEN PD4	/* o */
+#define DMXTX PD3	/* o */
+#define DMXRX PD2	/* i */
 
 int main(void) {
-	pinsetup();
-	ledhello();
+	
+	DDRB |= _BV(PB7) | _BV(PB6);
+	DDRB &= _BV(PB4);
+	PORTB &= ~_BV(PB7);
+	PORTB |= _BV(PB4);
+
+		/* enable reception, */
+		/* disable transmission */
+	DMXDDR |= _BV(DMXREN) | _BV(DMXTEN);
+	DMXPORT &= ~(_BV(DMXREN) | _BV(DMXTEN));
+
+		/* 1.25Mbd 8N1 */
+	UCSR0B |= _BV(TXEN0);
+	UCSR0C |= _BV(UCSZ01) | _BV(UCSZ00);
+	UBRR0H = 0;
+	UBRR0L = 0;
+	
+	while ((PINB & _BV(PB4)) != 0);
+
+	PORTB |= _BV(PB7);
+
+	asm volatile(
+			"															\
+			push	r16												\n\t\
+			push	r17												\n\t\
+			push	r18												\n\t\
+			ldi		r18,		8			; 1l		10l			\n\t\
+			ldi		r17,		0			; 1l		11l			\n\t\
+																		\
+loop_start:	in		r16,		%0			; 1			1			\n\t\
+			lsr		r16						; 1			2			\n\t\
+			lsr		r16						; 1			3			\n\t\
+			andi	r16,		1			; 1			4			\n\t\
+			or		r17,		r16			; 1			5			\n\t\
+			dec		r18						; 1			6			\n\t\
+			brne	not_last				; 1l/2n		7l/8n		\n\t\
+																		\
+last:		sts		%1,			r17			; 2l		9l			\n\t\
+			ldi		r18,		8			; 1l		10l			\n\t\
+			ldi		r17,		0			; 1l		11l			\n\t\
+			rjmp	loop_end				; 2l		13l			\n\t\
+																		\
+not_last:	lsl		r17						; 1l		9n			\n\t\
+			nop								; 1l		10n			\n\t\
+			nop								; 1l		11n			\n\t\
+			nop								; 1l		12n			\n\t\
+			nop								; 1l		13n			\n\t\
+																		\
+loop_end:	nop								; 1			14			\n\t\
+			rjmp	loop_start				; 2			16			\n\t\
+																		\
+			pop		r18												\n\t\
+			pop		r17												\n\t\
+			pop		r16												\n\t\
+			"
+			:
+			: "I" (_SFR_IO_ADDR(PIND)),
+				"" (UDR0)
+	);
+
 	for(;;);
+
 }
