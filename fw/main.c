341518629778299e0c43b279077dba948e818118
Leo Bärring <leo.barring@gmail.com>
2012-07-14 15:16:11 +0200

    only tests one thing (tx) but does it better
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- fw/main.c
+++ fw/main.c
@@ -1,45 +1,36 @@
-#include "board.h"
-#include <util/delay.h>
+#include <avr/io.h>
+#include "leds.h"
+#include "button.h"
 
-void isoliotest(void) {
-	for (uint8_t i=0; i<4; ++i) {
-		if ((i&2) != 0) {
-			/* receiver on, active low */
-			LED_ON(LED2);
-			DMXPORT &=~ _BV(DMXREN);
-		}
-		else {
-			/* receiver off */
-			LED_OFF(LED2);
-			DMXPORT |= _BV(DMXREN);
-		}
-		if ((i&1) != 0) {
-			/* transmitter on, active high */
-			LED_ON(LED1);
-			DMXPORT |= _BV(DMXTEN);
-		}
-		else {
-			/* transmitter off */
-			LED_OFF(LED1);
-			DMXPORT &=~ _BV(DMXTEN);
-		}
-		for (uint8_t j=0; j<20; ++j) {
-			DMXPORT &=~ ((j&1)<<DMXTX);
-			DMXPORT |= ((j&1)<<DMXTX);
-
-			if ((DMXPIN & _BV(DMXRX)) != 0)
-				LED_ON(LED0);
-			else
-				LED_OFF(LED0);
-			_delay_ms(50);
-		}
-	}
-}
-
+#define DMXDDR DDRD
+#define DMXPORT PORTD
+#define DMXPIN PIND
+#define DMXREN_ PD5
+#define DMXTEN PD4
+#define DMXTX PD3
+#define DMXRX PD2
 
 int main(void) {
-	pinsetup();
-	ledhello();
-	for(;;)
-		isoliotest();
+	led_setup();
+	button_setup();
+	DMXDDR |= _BV(DMXREN_)|_BV(DMXTEN)|_BV(DMXTX);
+	DMXDDR &= ~_BV(DMXREN_);
+
+	DMXPORT |= _BV(DMXTEN);
+	DMXPORT &= ~_BV(DMXREN_);
+
+	for (;;) {
+		if (button()) {
+			led_on(LED0);
+			DMXPORT |= _BV(DMXTX);
+		}
+		else {
+			led_off(LED0);
+			DMXPORT &= ~_BV(DMXTX);
+		}
+		if ((DMXPIN & _BV(DMXRX)) != 0)
+			led_on(LED1);
+		else
+			led_off(LED1);
+	}
 }
