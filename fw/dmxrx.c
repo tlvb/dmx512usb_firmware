225e55b4991944b05c3c90b514473930b4d67b89
Leo Bärring <leo.barring@gmail.com>
2012-08-21 17:03:43 +0200

    first pieces for dmx reception
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

---
+++ fw/dmxrx.c
@@ -0,0 +1,50 @@
+#include "dmxrx.h"
+
+volatile uint8_t dr_state;
+void (*dr_ucb)(uint8_t, uint8_t);
+
+void dmxrx_setup(void) {
+	/* 250k 8N2 */
+	/* interrupt on complete reception */
+
+	UCSR1B |= _BV(RXCIE1) | _BV(RXEN1);
+	UCSR1C |= _BV(USBS1) | _BV(UCSZ11) | _BV(UCSZ10);
+	dr_state = 255;
+
+}
+
+void dmxrx_register_update_callback(void (*ucb)(uint8_t, uint8_t)) {
+	dr_ucb = ucb;
+}
+
+ISR(USART1_RXC_vect) {
+	uint8_t bits = UCSR1A;
+	uint8_t byte = UDR1;
+	if ((bits & _BV(FE1)) != 0) {
+		/* SFB */
+		dr_state = DR_START;
+	}
+	else if (dr_state == DR_START) {
+		/* ignore packet if start code != zero */
+		if (byte == 0)
+			dr_state = 0;
+		else
+			dr_state = DR_IGNORE;
+	}
+	else if (dr_state < DR_BUFSZ) {
+		if (dr_ucb != NULL) {
+			(*dr_ucb)(dr_state, byte);
+		}
+		dr_rxb[dr_state++] = byte;
+	}
+	else {
+		dr_state = DR_IGNORE;
+	}
+}
+
+
+
+
+
+
+
---
+++ fw/dmxrx.h
@@ -0,0 +1,15 @@
+#ifndef __DMXRX_H__
+#define __DMXRX_H__
+
+
+#define DR_UBRRH 0
+#define DR_UBRRL 4
+#define DR_BUFSZ 64
+
+#define DR_IGNORE 254
+#define DR_ZERO 255
+
+void dmxrx_setup(void);
+void dmxrx_register_update_callback(void (*ucb)(uint8_t, uint8_t));
+
+#endif
