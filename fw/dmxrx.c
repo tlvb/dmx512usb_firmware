df741dbd6b267ef3cc10726e24981c68c501b128
Leo Bärring <leo.barring@gmail.com>
2012-08-21 17:15:05 +0200

    forgot includes
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- fw/dmxrx.c
+++ fw/dmxrx.c
@@ -1,6 +1,7 @@
 #include "dmxrx.h"
 
 volatile uint8_t dr_state;
+volatile uint8_t dr_rxb[DR_RXBSZ];
 void (*dr_ucb)(uint8_t, uint8_t);
 
 void dmxrx_setup(void) {
@@ -10,6 +11,7 @@ void dmxrx_setup(void) {
 	UCSR1B |= _BV(RXCIE1) | _BV(RXEN1);
 	UCSR1C |= _BV(USBS1) | _BV(UCSZ11) | _BV(UCSZ10);
 	dr_state = 255;
+	dr_ucb = 0;
 
 }
 
@@ -17,7 +19,7 @@ void dmxrx_register_update_callback(void (*ucb)(uint8_t, uint8_t)) {
 	dr_ucb = ucb;
 }
 
-ISR(USART1_RXC_vect) {
+ISR(USART1_RX_vect) {
 	uint8_t bits = UCSR1A;
 	uint8_t byte = UDR1;
 	if ((bits & _BV(FE1)) != 0) {
@@ -31,8 +33,8 @@ ISR(USART1_RXC_vect) {
 		else
 			dr_state = DR_IGNORE;
 	}
-	else if (dr_state < DR_BUFSZ) {
-		if (dr_ucb != NULL) {
+	else if (dr_state < DR_RXBSZ) {
+		if (dr_ucb != 0) {
 			(*dr_ucb)(dr_state, byte);
 		}
 		dr_rxb[dr_state++] = byte;
--- fw/dmxrx.h
+++ fw/dmxrx.h
@@ -1,13 +1,16 @@
 #ifndef __DMXRX_H__
 #define __DMXRX_H__
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 
 #define DR_UBRRH 0
 #define DR_UBRRL 4
-#define DR_BUFSZ 64
+#define DR_RXBSZ 64
 
 #define DR_IGNORE 254
-#define DR_ZERO 255
+#define DR_START 255
 
 void dmxrx_setup(void);
 void dmxrx_register_update_callback(void (*ucb)(uint8_t, uint8_t));
