0023a51de52eebbce453a4720bc20086e424a57c
Leo Bärring <leo.barring@gmail.com>
2012-09-27 13:50:59 +0200

    test firmware for controlling nexa remote with channel 0-3
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- fw/main.c
+++ fw/main.c
@@ -1,46 +1,81 @@
-#include "leds.h"
-#include "usbusart.h"
 #include "dmxrx.h"
+#include "leds.h"
 
-volatile uint8_t cb_heartbeat;
+
+#define nexa_on(x) PORTB |= _BV(x)
+#define nexa_off(x) PORTB &= ~_BV(x)
+
+
+volatile uint8_t nexa_target;
+uint8_t nexa_state;
+
 
 void update(uint8_t channel, uint8_t value) {
-	cb_heartbeat = 1;
-	uint8_t bytes[2] = { channel, value };
-	uu_write(bytes, 2);
+	if (channel == 0) {
+		if (value > 0x7f)
+			nexa_target |= 1;
+		else 
+			nexa_target &= 2;
+	}
+	else if (channel == 1) {
+		if (value > 0x7f)
+			nexa_target |= 2;
+		else
+			nexa_target &= 1;
+	}
 }
 
 
 
 int main(void) {
 	led_setup();
-	uu_setup();
 	dr_setup();
 	dr_register_update_callback(&update);
-	cb_heartbeat = 0;
 
 	led_hello();
 				//	 123456789abcdef012
-	uint8_t msg[] = "receiver firmware\n";
 
+	DDRB |= 0x0f;
+	nexa_off(0);
+	nexa_off(1);
+	nexa_off(2);
+	nexa_off(3);
 
 	sei();
 
-	uu_write(msg, 0x12);
-
 	for(;;) {
-		if (dr_heartbeat != 0 || cb_heartbeat != 0 ) {
-			uint8_t copy = dr_heartbeat | (cb_heartbeat << 1);
-			dr_heartbeat = cb_heartbeat = 0;
-			if (copy & 1)
-				led_on(LED0);
-			if (copy & 2)
-				led_on(LED1);
-			_delay_ms(10);
-			led_off(LED0);
-			led_off(LED1);
-			_delay_ms(490);
+		int8_t i = -1;
+		if ((nexa_target & 1) != (nexa_state & 1)) {
+			if ((nexa_target & 1) != 0) {
+				i = 0;
+				nexa_state |= 1;
+			}
+			else {
+				i = 1;
+				nexa_state &= 2;
+			}
 		}
+		else if ((nexa_target & 2) != (nexa_state & 2)) {
+			if ((nexa_target & 2) != 0) {
+				i = 2;
+				nexa_state |= 2;
+			}
+			else {
+				i = 3;
+				nexa_state &= 1;
+			}
+		}
+		if (i >= 0) {
+			nexa_on(i);
+			led_on(LED0);
+			_delay_ms(100);
+			led_off(LED0);
+			_delay_ms(100);
+			nexa_off(i);
+		}
+		
+
+
 	}
 
 }
